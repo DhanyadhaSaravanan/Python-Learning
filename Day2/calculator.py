e0095ce660dec750c23526eb14b25df63bfcc82b
DhanyadhaSaravanan <dhanyadhaksaravanan@gmail.com>
2026-07-15 19:43:44 +0530

    Day 2:Python Basics

---
+++ Day2/calculator.py
@@ -0,0 +1,7 @@
+a=5
+b=4
+print("The sum of", a, "and", b, "is:", a+b)
+print("The difference of", a, "and", b, "is:", a-b)
+print("The product of", a, "and", b, "is:", a*b)
+print("The quotient of", a, "and", b, "is:", a/b)
+print("The remainder of", a, "and", b, "is:", a%b)
---
+++ Day2/largest.py
@@ -0,0 +1,9 @@
+a=15
+b=5
+c=7
+if(a>b and a>c):
+    print("The largest number is:", a)
+elif(b>a and b>c):
+    print("The largest number is:", b)
+else:
+    print("The largest number is:", c)
